b46a4e7f97076754745b6873c03b4c5d18724972
amine <bougassaa@gmail.com>
2023-05-18 15:05:51 +0200

    use attribute for svg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let SKY_SVG_SCALED = false;
-
 document.addEventListener('DOMContentLoaded', function () {
     registerBackgroundSky();
     registerTitleWelcome();
@@ -37,33 +35,9 @@ function registerBackgroundSky() {
     ['data_ready', 'DOMLoaded'].forEach(event => {
         animation.addEventListener(event, () => {
             let svg = container.querySelector('svg');
-            if (!SKY_SVG_SCALED && svg instanceof Element) {
-                SKY_SVG_SCALED = true;
-                let height = scaleLottieObject(1900, 1080, svg);
-                container.style.height = height + 'px';
+            if (svg instanceof Element) {
+                svg.setAttribute('preserveAspectRatio', 'xMidYMid slice')
             }
         });
     });
-}
-
-function scaleLottieObject(svgWidth, svgHeight, svgElement) {
-    const windowWidth = window.innerWidth, windowHeight = window.innerHeight,
-        windowRatio = windowWidth / windowHeight, svgRatio = svgWidth / svgHeight;
-    let newWidth, newHeight;
-
-    if (windowRatio > svgRatio) {
-        // L'écran est plus large que l'image = desktop
-        newWidth = windowWidth;
-        newHeight = (windowWidth / svgRatio) - 12;
-    } else {
-        // L'écran est plus haut que l'image = mobile
-        newWidth = (windowHeight * svgRatio) + 20;
-        newHeight = windowHeight;
-    }
-
-    svgElement.style.transform = `translate(-${(newWidth/2) - (windowWidth / 2)}px, -${(newHeight/2) - (windowHeight / 2)}px`;
-    svgElement.style.width = newWidth;
-    svgElement.style.height = newHeight;
-
-    return newHeight;
 }
